219da164f1f5032524a27a79f76d083075f1b0d0
LAB22_USER ON WS06 <L22X16W06@LabHCI.net>
2026-07-01 17:59:05 +0800

    initial

---
+++ backend/stockmate-backend/stockmate-backend/src/main/java/com/stockmate/stockmate_backend/StockmateApplication.java
@@ -0,0 +1,13 @@
+package com.stockmate.stockmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmateBackendApplication.class, args);
+	}
+
+}
