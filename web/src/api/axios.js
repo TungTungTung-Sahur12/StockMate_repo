181694a8e0fcdd745484290f29c051d9c56c90b8
AlexLouiseCortesOOP1 <alexlouise.cortes@cit.edu>
2026-07-16 13:15:20 +0800

    Feature: global 401 handler to force re-login on expired token

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -4,4 +4,16 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("stockmate_auth");
+      delete api.defaults.headers.common["Authorization"];
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
